cfbea3478cbad10681e4225141fbf28f483b04f8
Maksym Zhuk <mzmzhuk@gmail.com>
2024-04-14 11:02:15 +0300

    added tests_passed photos

--- Implement Queue using Stacks/Implement Queue using Stacks.py
+++ Implement Queue using Stacks/Implement Queue using Stacks.py
@@ -1,13 +1,49 @@
 """Queue implementation using two stacks"""
-from collections import deque
+
+
+class Node:
+    """Node"""
+
+    def __init__(self, item: int) -> None:
+        """Initialize node"""
+        self.item = item
+        self.next = None
+
+
+class Stack:
+    """Stack"""
+
+    def __init__(self) -> None:
+        """Initialize stack"""
+        self.top = None
+
+    def __bool__(self) -> bool:
+        """Return bool of stack"""
+        return bool(self.top)
+
+    def append(self, item: int) -> None:
+        """append item to the top of stack"""
+        top = Node(item)
+        top.next = self.top
+        self.top = top
+
+    def pop(self) -> int:
+        """Pop element from top of the stack"""
+        top = self.top
+        self.top = top.next
+        return top.item
+
+    def peek(self) -> int:
+        """Peek element from the top of the stack"""
+        return self.top.item
 
 
 class MyQueue:
     """Queue implemented with two queues"""
 
     def __init__(self):
-        self.direct_stack = deque()
-        self.reversed_stack = deque()
+        self.direct_stack = Stack()
+        self.reversed_stack = Stack()
 
     def push(self, x: int) -> None:
         """Push x to the top of queue"""
@@ -25,7 +61,7 @@ class MyQueue:
         """Peek element from end of queue"""
         if not self.reversed_stack:
             self._copy_from_direct_to_reversed()
-        return self.reversed_stack[-1]
+        return self.reversed_stack.peek()
 
     def empty(self) -> bool:
         """Return True if queue is empty"""
@@ -40,3 +76,13 @@ class MyQueue:
         """Copy all data from reversed to direct stacks"""
         while self.reversed_stack:
             self.direct_stack.append(self.reversed_stack.pop())
+
+
+q = MyQueue()
+q.push(1)
+q.push(2)
+q.push(3)
+print(q.peek())
+print(q.pop())
+print(q.pop())
+print(q.pop())
--- Implement Stack Using Queues/Implement Stack using Queues.py
+++ Implement Stack Using Queues/Implement Stack using Queues.py
@@ -1,5 +1,48 @@
 """Stack implementation using two queues"""
 
+
+class Node:
+    """Node"""
+
+    def __init__(self, item: int) -> None:
+        """Initialize node"""
+        self.item = item
+        self.next = None
+        self.prev = None
+
+
+
+class Queue:
+    """Queue implementation"""
+
+    def __init__(self) -> None:
+        """Initialize queue"""
+        self.front = None
+        self.back = None
+
+    def push(self, item: int) -> None:
+        """Push element to back of the queue"""
+        if not self.back:
+            self.front = self.back = Node(item)
+            return
+
+        back = Node(item)
+        back.next = self.back
+        self.back.prev = back
+        self.back = back
+
+    def pop(self) -> int:
+        """Pop element from front of queue"""        
+        front = self.front
+        self.front = self.front.prev
+        self.front.next = None
+        return front.item
+
+    def peek(self) -> int:
+        """Peek element from front of queue"""
+        return self.front.item
+
+
 class MyStack:
     """Stack implementation using two queues"""
 
